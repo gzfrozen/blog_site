4ef46c95a6081e9f77c82407b848f8ffd5c0e87b
gzfrozen <gzliuxiaochen@gmail.com>
2019-12-06 22:01:21 +0900

    Add models in blog app.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.name
+
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    description = models.TextField()
+    creat_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    is_public = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-creat_at']
+
+    def save(self, *args, **kwargs):
+        if self.is_public and not self.published_at:
+            self.published_at = timezone.now()
+        super.save(*args, **kwargs)
+
+    def _str_(self):
+        return self.title

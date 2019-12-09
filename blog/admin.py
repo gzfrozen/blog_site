99693b3325cc3d0cde5e6a70c97ae76bef8dad63
gzfrozen <gzliuxiaochen@gmail.com>
2019-12-09 15:35:59 +0900

    Add admin site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from blog.models import Category, Tag, Post
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     slug = models.SlugField(unique=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 
@@ -16,7 +16,7 @@ class Tag(models.Model):
     slug = models.SlugField(unique=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
 
@@ -37,7 +37,7 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if self.is_public and not self.published_at:
             self.published_at = timezone.now()
-        super.save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
-    def _str_(self):
+    def __str__(self):
         return self.title

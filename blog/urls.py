e0339e7c350fec01b77e1497c08327d45af24a83
gzfrozen <gzliuxiaochen@gmail.com>
2019-12-10 21:06:28 +0900

    Add templates and views , use extend template.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog.views import IndexView, PostDetailView, CategoryListView, TagListView
+from blog.views import IndexView, PostDetailView, CategoryListView, CategoryPostView, TagListView, TagPostView
 
 app_name = 'blog'
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
     path('tags/', TagListView.as_view(), name='tag_list'),
+    path('category/<str:category_slug>',
+         CategoryPostView.as_view(), name='category_post'),
+    path('tag/<str:tag_slug>', TagPostView.as_view(), name='tag_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Count, Q
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
@@ -28,6 +29,37 @@ class CategoryListView(ListView):
         num_posts=Count('post', filter=Q(post__is_public=True)))
 
 
+class CategoryPostView(ListView):
+    model = Post
+    template_name = 'blog/category_post.html'
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        self.category = get_object_or_404(
+            Category, slug=category_slug)
+        return super().get_queryset().filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+
 class TagListView(ListView):
     queryset = Tag.objects.annotate(num_posts=Count(
         'post', filter=Q(post__is_public=True)))
+
+
+class TagPostView(ListView):
+    model = Post
+    template_name = 'blog/tag_post.html'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug']
+        self.tag = get_object_or_404(Tag, slug=tag_slug)
+        return super().get_queryset().filter(tags=self.tag)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context

4d0f4eb9c249900f8874e2ac0b5852cba36c4af7
gzfrozen <gzliuxiaochen@gmail.com>
2019-12-17 01:29:40 +0900

    Add search form.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog.views import IndexView, PostDetailView, CategoryListView, CategoryPostView, TagListView, TagPostView
+from blog.views import IndexView, PostDetailView, CategoryListView, CategoryPostView, TagListView, TagPostView, SearchPostView
 
 app_name = 'blog'
 urlpatterns = [
@@ -11,4 +11,6 @@ urlpatterns = [
     path('category/<str:category_slug>',
          CategoryPostView.as_view(), name='category_post'),
     path('tag/<str:tag_slug>', TagPostView.as_view(), name='tag_post'),
+    path('search/',
+         SearchPostView.as_view(), name='search_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -72,3 +72,27 @@ class TagPostView(ListView):
         context = super().get_context_data(**kwargs)
         context['tag'] = self.tag
         return context
+
+
+class SearchPostView(ListView):
+    model = Post
+    template_name = 'blog/search_post.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', None)
+        lookups = (Q(title__icontains=query) |
+                   Q(content__icontains=query) |
+                   Q(category__name__icontains=query) |
+                   Q(tags__name__icontains=query))
+        if query is not None:
+            qs = super().get_queryset().filter(lookups).distinct()
+            return qs
+        qs = super().get_queryset()
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context

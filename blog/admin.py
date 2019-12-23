fbc91a5a86c1abc87689afa36738551d154b348a
gzfrozen <gzliuxiaochen@gmail.com>
2019-12-23 18:00:30 +0900

    Minor change.

--- blog/admin.py
+++ blog/admin.py
@@ -9,9 +9,26 @@ class ContentImageInline(admin.TabularInline):
     extra = 1
 
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+class ReplyInline(admin.TabularInline):
+    model = Reply
+    extra = 1
+
+
 class PostAdmin(admin.ModelAdmin):
     inlines = [
         ContentImageInline,
+        CommentInline,
+    ]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = [
+        ReplyInline,
     ]
 
 
@@ -19,5 +36,5 @@ admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
 admin.site.register(ContentImage)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Reply)
